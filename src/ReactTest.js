671097c78c6cd9e18641579f4beb7513c62e2924
jbockerstette <jim.bockerstette@gmail.com>
2018-02-06 09:14:23 -0800

    Saving the eslint settings trying to get  html work

---
+++ src/ReactTest.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class ReactTest extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gretting: 'hi'
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <span>hi</span>
+        <div>{this.state.gretting}</div>
+      </div>
+    );
+  }
+}
+
+export default ReactTest;
